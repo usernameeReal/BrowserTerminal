a769da6fdb8131e2acc9af8d9cc56704919f5888
usernamee <86745494+usernameeReal@users.noreply.github.com>
2022-07-08 18:37:50 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 set -e
+sudo apt update && sudo apt install zip -y
 npm i
 cd static/
 npm i
 cd ..
 npm i -g nexe@4.0.0-rc.1
-cp node_modules/node-pty/build/Release/pty.node build/ || cp node_modules/node-pty/build/Debug/pty.node build/
+rm build/node_modules -rm || true
+mkdir -p build/node_modules/node-pty/build/Release/
+
+cp node_modules/node-pty/build/Release/pty.node build/build/node_modules/node-pty/build/Release/
 nexe -t x64-12.16.2 -r static/ -o browsertty index.js
+zip -r BrowserTerminal.zip build/
+mv BrowserTerminal.zip build/

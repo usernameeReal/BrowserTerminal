e14bcff8bc1fc725bf38b6f87a152a166f52f4e1
usernamee <86745494+usernameeReal@users.noreply.github.com>
2022-07-08 18:29:56 +0200

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 git config --global user.email "noone@example.com"
 git config --global user.name "Actions Build"
 mv browsertty build/BrowserTerminal
-cp build/rdm.dat build/README.md
+cat build/rdm.dat | | perl -pe 'chomp if eof' > build/README.md
 date >> build/README.md
 git add build/*
 git commit -m "Add build on $(date)"

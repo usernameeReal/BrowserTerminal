c3e6cff875a20587016884ae2fdcb4b5fda176f3
usernamee <86745494+usernameeReal@users.noreply.github.com>
2022-07-08 18:08:59 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ cd static/
 npm i
 cd ..
 npm i -g nexe@4.0.0-rc.1
-nexe -t x64-12.16.2 -r static/ -o browsertty index.js
+cp node_modules/node-pty/build/Release/pty.node build/ || cp node_modules/node-pty/build/Debug/pty.node build/
+nexe -t x64-12.16.2 -r static/ -r  -o browsertty index.js

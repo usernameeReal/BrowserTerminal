0408e0398630cc07f27e4965c9c890beea68fcbd
usernamee <86745494+usernameeReal@users.noreply.github.com>
2022-07-08 18:09:37 +0200

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -3,5 +3,6 @@ git config --global user.email "noone@example.com"
 git config --global user.name "Actions Build"
 mv browsertty build/BrowserTerminal
 git add build/BrowserTerminal
+git add build/pty.node
 git commit -m "Add build on $(date)"
 git push
